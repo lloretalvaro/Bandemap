8d221edc4a9c6240d50af4ff9e2681a0e8932d21
Lloret López álvaro <alvarolloretlopez@gmail.com>
2023-08-01 14:19:28 +0200

    primer commit app alvaro lloret

---
+++ Bandemap/BandemapApp.swift
@@ -0,0 +1,17 @@
+//
+//  BandemapApp.swift
+//  Bandemap
+//
+//  Created by Lloret López álvaro on 1/8/23.
+//
+
+import SwiftUI
+
+@main
+struct BandemapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
